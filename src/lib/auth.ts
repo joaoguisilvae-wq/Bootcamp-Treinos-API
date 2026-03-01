0388d45c42330c64219bf20b69fd28d8a29c3ab2
João <joaogui.silvae@gmail.com>
2026-03-01 00:40:25 -0300

    chore: add better auth setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,19 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { openAPI } from "better-auth/plugins";
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+});
+
+export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  plugins: [openAPI()],
+});
